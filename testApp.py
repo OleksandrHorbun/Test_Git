22f411a763575b32dd985428f7fddefa97886903
OleksandrHorbun <drawkey28@gmail.com>
2022-12-24 07:23:09 +0200

    First commit in testApp

---
+++ testApp.py
@@ -0,0 +1,7 @@
+class testApp:
+    print ("Hello World!")
+    print ("This is testApp for a Git")
+
+    file = open('readme.txt', 'r', encoding="utf-8")
+    print("\n", file.read())
+    file.close()
